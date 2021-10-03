12e8f4907a9b5aa3aa05ca5fe4daf11e505ad1e8
José Fernando Pereira <jose.ze.fernando@gmail.com>
2021-10-03 18:15:15 -0300

    Cópia de segurança

---
+++ src/test/java/br/com/recatalog/parser/visualbasic6/AppTest.java
@@ -0,0 +1,22 @@
+package br.com.recatalog.parser.visualbasic6;
+
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue(true);
+    }
+}
